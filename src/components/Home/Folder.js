0da688c0e231ad3e7c54835530e20e1d9da86b48
aiacuone <aiacuone@users.noreply.github.com>
2021-08-05 08:10:30 +0100

    chip at home page

--- src/components/Home/Folder.js
+++ src/components/Home/Folder.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import menu_button from 'images/menu.svg'
+import Chip from '@material-ui/core/Chip'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import { useState } from 'react'
+import Popover from '@material-ui/core/Popover'
 
 export default function Folder({ state, setState, vars, folder }) {
-	function handleClick(e) {
-		let clickedFolderOrSettings =
-			e.target.className == 'home_folder'
-				? 'folder'
-				: e.target.className == 'home_folder_menu_button' && 'settings'
+	const [anchorEl, setAnchorEl] = useState(null)
 
-		if (clickedFolderOrSettings == 'folder') {
-			handleClickFolder()
-		}
-		if (clickedFolderOrSettings == 'settings') {
-			handleClickSettings()
-		}
-	}
+	// function handleClick(e) {
+
+	// 	let clickedFolderOrSettings =
+	// 		e.target.className == 'home_folder'
+	// 			? 'folder'
+	// 			: e.target.className == 'home_folder_menu_button' && 'settings'
+
+	// 	if (clickedFolderOrSettings == 'folder') {
+	// 		handleClickFolder()
+	// 	}
+	// 	if (clickedFolderOrSettings == 'settings') {
+	// 		handleClickSettings()
+	// 	}
+	// }
 
 	function handleClickFolder() {
 		let newFolders = { ...state.folders }
@@ -28,13 +35,47 @@ export default function Folder({ state, setState, vars, folder }) {
 		setState.setHomeRender(['folderSettings', folder.folderColor, folder.name])
 	}
 
+	function handleDelete() {
+		console.log('delete')
+	}
+
+	const handleClick = (e) => {
+		!e.target.textContent
+			? setAnchorEl(e.currentTarget)
+			: console.log('clicked')
+	}
+
+	const handleClose = () => {
+		setAnchorEl(null)
+	}
+
+	const open = Boolean(anchorEl)
+	const id = open ? 'simple-popover' : undefined
+
 	return (
-		<p
-			className="home_folder"
-			style={{ background: folder.folderColor }}
-			onClick={handleClick}>
-			{folder.name.toUpperCase()}
-			<img className="home_folder_menu_button" src={menu_button} />
-		</p>
+		<>
+			<Chip
+				className="home_folder"
+				label={folder.name.toUpperCase()}
+				onDelete={handleDelete}
+				icon={<MoreVertIcon style={{ color: 'white' }} />}
+				style={{ background: folder.folderColor }}
+				onClick={handleClick}></Chip>
+
+			<Popover
+				open={open}
+				anchorEl={anchorEl}
+				onClose={handleClose}
+				anchorOrigin={{
+					vertical: 'top',
+					horizontal: 'center',
+				}}
+				transformOrigin={{
+					vertical: 'bottom',
+					horizontal: 'center',
+				}}>
+				The content of the Popover.
+			</Popover>
+		</>
 	)
 }
